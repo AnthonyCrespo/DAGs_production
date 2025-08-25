4156a3524efb65bb93ba9a875f2651bce28fdcb6
AnthonyCrespo <anthony.crespoy@gmail.com>
2025-08-25 12:37:27 -0500

    Modified DAG 1

---
+++ dags/dag_1.py
@@ -0,0 +1,18 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime
+
+
+dag = DAG(
+    'mi_dag_desde_git_1',
+    description='DAG cargado desde Git Bundle 1',
+    schedule_interval='@daily',
+    start_date=datetime(2024, 1, 1),
+    catchup=False
+)
+
+task1 = BashOperator(
+    task_id='tarea_desde_git',
+    bash_command='echo "Ejecutando desde Git Bundle 1!"',
+    dag=dag
+)
